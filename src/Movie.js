30b89d96276786a1f403a1de2679e67f37f7991e
ibmike87 <ibmike87@gmail.com>
2021-06-23 23:52:16 +0900

    movie

---
+++ src/Movie.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+Movie.prototype = {};
+
+function Movie() {
+    return <h1></h1>;
+}
+
+Movie.propTypes = {};
+
+export default Movie;
